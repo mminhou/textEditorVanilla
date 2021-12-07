a286bae0ad447c42ccfe146f68c9e0e8ac52439b
R _ R <exit19093@gmail.com>
2021-12-07 09:30:41 +0900

    Feat: tab list componnet 정의

--- src/component/TabList.ts
+++ src/component/TabList.ts
@@ -1,7 +1,8 @@
-export default function TabList({$app, initialState, closeTabBtn}) {
-    this.state = initialState;
+export default function TabList({$app, closeTabBtn, selectTab, activeTab}) {
+    this.state = JSON.parse(localStorage.getItem('tabs'));
     this.$target = document.createElement('div');
     this.closeTabBtn = closeTabBtn;
+    this.selectTab = selectTab;
     $app.appendChild(this.$target);
 
     this.setState = (nextState) => {
@@ -12,24 +13,31 @@ export default function TabList({$app, initialState, closeTabBtn}) {
     this.render = () => {
         const tabs = this.state.map((tab) => {
             return `
-                <div style="display: inline-block; width: 100px;">
+                <div class="selectedTab" data-name="${tab}"
+                     style="display: inline-block; width: 100px; border: 1px solid black; padding: 10px;">
                     ${tab}
-                    <button>x</button>
                 </div> 
+                <button class="closeTabBtn" data-name="${tab}">x</button>
             `
         }).join('');
 
         this.$target.innerHTML = `
             <div style="width: 100%; display: inline-block;">
-                ${tabs}
+                ${tabs} 
             </div>
         `
 
-
         this.$target.addEventListener('click', (e) => {
-            if (e.target.closest('.closeTabBtn')) {
-                this.closeTabBtn();
+            const selected = e.target.closest('.selectedTab')
+            if (selected) {
+                this.selectTab(selected.dataset.name);
+                return
+            }
 
+            const closeTarget = e.target.closest('.closeTabBtn')
+            if (closeTarget) {
+                this.closeTabBtn(closeTarget.dataset.name)
+                return
             }
         });
 
