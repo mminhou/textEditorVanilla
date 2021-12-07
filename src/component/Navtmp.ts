af6fa5bc6689755dfb291869ec7b41e777cd0d32
R _ R <exit19093@gmail.com>
2021-12-07 10:48:31 +0900

    Feat: react component 구현작업

--- src/component/Navtmp.ts
+++ src/component/Navtmp.ts
@@ -1,3 +1,5 @@
+import Button from "@material-ui/core";
+
 export default function Nav({$app, addTabBtn, saveBtn}) {
     this.$target = document.createElement('div');
     this.addTabBtn = addTabBtn;
@@ -12,8 +14,16 @@ export default function Nav({$app, addTabBtn, saveBtn}) {
     this.render = () => {
         this.$target.innerHTML = `               
             <h1>Navigation</h1>
-            <button class="add">add</button>
-            <button class="save">save</button>
+            <button>새파일</button>
+            <div>
+                <button class="load">로드</button>
+                <ul>
+                    <li>Sub Item 1</li>
+                    <li>Sub Item 2</li>
+                </ul>
+            </div>
+            <button>저장</button>
+            <button>다른이름으로 저장</button>
         `
 
         this.$target.addEventListener('click', (e) => {
@@ -23,6 +33,14 @@ export default function Nav({$app, addTabBtn, saveBtn}) {
             }
             if (e.target.closest('.save')) {
                 this.saveBtn();
+                return
+            }
+            if (e.target.closest('.load')) {
+                console.log('load data')
+                var parent = e.parentElement;
+                var dd = parent.lastChild.previousSibling;
+                dd.classList.toggle('show');
+                console.log(localStorage)
                 return
             }
         });
