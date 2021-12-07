0840c4c80826a6b47beb77e180a52b55bf9028c7
R _ R <exit19093@gmail.com>
2021-12-07 17:40:30 +0900

    Feat: new tab 버튼 -> create() 정의

--- src/component/Notepad.ts
+++ src/component/Notepad.ts
@@ -3,6 +3,7 @@ import Tab from "./Tab";
 export default class Notepad {
     $target = document.createElement('div')
     tabs: Tab[] = [];
+    cnt: number = 0;
 
     constructor($parent) {
         $parent.appendChild(this.$target);
@@ -10,37 +11,55 @@ export default class Notepad {
     }
 
     init() {
-        let tab1 = new Tab()
+        let tab1 = new Tab(this.cnt++)
         this.tabs.push(tab1)
-        this.tabs.push(tab1)
+        let tab2 = new Tab(this.cnt++)
+        this.tabs.push(tab2)
         this.render();
     }
 
     template() {
         const tabList = this.tabs.map((tab) => {
             return `
-                ${tab}
+                <p style="display: inline-block; width: 150px; border-left: 1px solid black; border-right: 1px solid black; margin: 0;">
+                    ${tab.getTabName()}
+                    <span class="close-btn" data-title="${tab.getTabName()}" style="float: right; padding-right: 10px;">x</span>
+                </p>
             `
         }).join('')
 
 
         return `
             <p>notepad</p>
-            ${tabList}
-
+            <div class="tab-list" style="border: 1px solid black">
+                ${tabList}
+            <div>
         `
     }
 
     render() {
         this.$target.innerHTML = this.template();
+        this.$target.addEventListener('click', (e) => {
+            const closeBtn = (e.target as Element).closest('.close-btn')
+            console.log(closeBtn)
+
+        })
+
     }
 
     create() {
-        let tab = new Tab();
+        let tab = new Tab(this.cnt++);
         this.tabs.push(tab);
         this.render();
     }
 
+    close(tabName: string) {
+        const targetTab = this.tabs.find(e => e.title === tabName);
+        // this.tabs.splice(targetTab, 1)
+        console.log(targetTab);
+
+        this.render();
+    }
 
 
 }
