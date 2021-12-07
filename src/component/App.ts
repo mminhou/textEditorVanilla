9c90e85a83dbcbec730af610c7697403a923f9ab
R _ R <exit19093@gmail.com>
2021-12-07 09:29:21 +0900

    Feat: add, close tab 기능 추가

--- src/component/App.ts
+++ src/component/App.ts
@@ -1,31 +1,69 @@
 import Nav from "./Nav";
 import TabList from "./TabList";
+import Tab from "./Tab";
 // class notepad
 
 export default function App($app) {
-    this.state = {
-        tabs: ['tab1', 'tab2', 'tab3']
-    };
-
+    this.activeTab = 'tab1';
 
     this.addTabBtn = () => {
-        this.state.tabs.push('1');
-        tabList.setState(this.state.tabs);
+        if (localStorage.getItem('tabs') === '[]') {
+            localStorage.setItem('tabs', JSON.stringify(['tab1']));
+            localStorage.setItem('tab1', 'tab1content');
+            tabList.setState(['tab1']);
+            return
+        }
+        const tmp = JSON.parse(localStorage.getItem('tabs'));
+        const lastTab = tmp[tmp.length-1];
+        const lastNum = parseInt(lastTab.replace( /[^0-9]/g, ""))+1;
+        tmp.push('tab'+lastNum);
+        localStorage.setItem('tabs', JSON.stringify(tmp));
+        localStorage.setItem('tab'+lastNum, 'tab'+lastNum+'content');
+        tabList.setState(tmp);
     };
 
-    this.closeTabBtn = () => {
+    this.closeTabBtn = (targetName) => {
+        console.log(targetName)
+        const tmp = JSON.parse(localStorage.getItem('tabs'));
+        if (tmp.includes(targetName)) {
+            const targetId = tmp.indexOf(targetName);
+            tmp.splice(targetId, 1);
+            localStorage.setItem('tabs', JSON.stringify(tmp));
+            tabList.setState(tmp);
+            tab.setState('');
+        }
+    }
+
+    this.selectTab = (selectedTabName) => {
+        // const selectedTabData = localStorage.getItem(selectedTabName);
+        // console.log(selectedTabData);
+        tab.setState(selectedTabName);
+        tab.render();
+    }
+
+    this.saveBtn = () => {
+        localStorage.setItem('tabs', this.state.tabs);
+        console.log(localStorage.getItem('tabs'));
 
     }
 
     const nav = new Nav({
         $app,
-        addTabBtn: this.addTabBtn
+        addTabBtn: this.addTabBtn,
+        saveBtn: this.saveBtn
     });
+
     const tabList = new TabList({
         $app,
-        initialState: this.state.tabs,
-        closeTabBtn: this.closeTabBtn
+        closeTabBtn: this.closeTabBtn,
+        selectTab: this.selectTab,
+        activeTab: this.activeTab
     });
+
+    const tab = new Tab({
+        $app,
+        initialState: localStorage.getItem(this.activeTab)
+    })
 
 
 };
